0943e5903d0064307c68046d5cdd503d9c4646ad
CarlosHernandez14 <carloskvpchc@gmail.com>
2024-11-28 07:58:12 -0600

    First commit

---
+++ src/main/java/com/mycompany/repartidoraagua/RepartidoraAgua.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repartidoraagua;
+
+/**
+ *
+ * @author carlo
+ */
+public class RepartidoraAgua {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
